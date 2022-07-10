0d7d98a3d79cb118ed9c0435650d531be6f400a3
RayaneKettana <rkettana@gmail.com>
2022-07-10 23:33:59 +0200

    Implementation of the Composite Pattern

---
+++ CompositePattern/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CompositePattern
+{
+    static class Program
+    {
+        public static void Main()
+        {
+
+            var breakfast = new Menu("Breakfast", "Chinese Pancake");
+            var lunch = new Menu("Lunch", "Couscous Food");
+            var dinner = new Menu("Dinner", "Italian food");
+
+            var dessert = new Menu("Dessert", "Ice Cream");
+
+            var menu = new Menu("All", "Burger King");
+
+            breakfast.Add(new MenuItem("Waffles", "Sugar waffles", 60, false));
+            breakfast.Add(new MenuItem("Corn Flakes", "Kellogs", 30, true));
+
+            lunch.Add(new MenuItem("Burger", "Cheese and Rings Burger", 100, true));
+            lunch.Add(new MenuItem("Sandwich", "Fish Sandwich", 45, false));
+
+            dinner.Add(new MenuItem("Pizza", "4 cheese", 22, true));
+            dinner.Add(new MenuItem("Pasta", "Fish Pasta", 12, false));
+
+            dessert.Add(new MenuItem("Ice Cream", "Vanilla and Coco", 49, true));
+            dessert.Add(new MenuItem("Cake", "Tiramisu", 30, false));
+
+            dinner.Add(dessert);
+            menu.Add(breakfast);
+            menu.Add(lunch);
+            menu.Add(dinner);
+
+            menu.Print();
+
+        }
+    }
+}
---
+++ CompositePattern/README.md
@@ -0,0 +1,8 @@
+## Composite Pattern
+Structural design pattern
+
+The Composite design pattern is a structural design pattern that allows objects to be arranged in trees so that they can be treated as individual objects.
+
+This pattern provides a framework for designing a composition of objects whose depth is unknown (we can use a tree as an analogy).
+
+The "clients" interact with the objects without knowing the structure of the tree.![](../images/composite.png)![](../images/composite_generique.png)
